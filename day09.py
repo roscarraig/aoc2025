c3d06fdc146350af207d5a7e91f681ba804104f2
Paul Mc Auley <paul.mcauley@workday.com>
2025-12-11 11:50:22 +0000

    Day 9 completed

--- day09.py
+++ day09.py
@@ -20,7 +20,6 @@ def pdir(a, b):
         return 2
     if a[1] > b[1]:
         return 3
-    print("This should not happen")
     return 0
 
 
@@ -33,43 +32,120 @@ def turn(a, b):
         return -1
     return 1
 
-def isopposite(tiles, dirs, a, b):
+
+def corner(tiles, p):
+    """ Determine the orientation of a corner """
+
+    result = 0
+    n = len(tiles)
+    a = tiles[(p - 1) % n]
+    b = tiles[p]
+    c = tiles[(p + 1) % n]
+
+    if a[0] == b[0] and b[1] > a[1] and c[0] < b[0]:
+        result = 0
+    elif a[0] == b[0] and b[1] > a[1] and c[0] > b[0]:
+        result = 5
+    elif a[0] == b[0] and b[1] < a[1] and c[0] < b[0]:
+        result = 6
+    elif a[0] == b[0] and b[1] < a[1] and c[0] > b[0]:
+        result = 3
+    elif a[1] == b[1] and b[0] > a[0] and c[1] > b[1]:
+        result = 2
+    elif a[1] == b[1] and b[0] > a[0] and c[1] < b[1]:
+        result = 4
+    elif a[1] == b[1] and b[0] < a[0] and c[1] > b[1]:
+        result = 7
+    elif a[1] == b[1] and b[0] < a[0] and c[1] < b[1]:
+        result = 1
+
+    return result
+
+
+def isopposite(tiles, corners, i, j):
     """ Have validated that the data set is moving clockwise """
 
+    if tiles[i][0] < tiles[j][0]:
+        a = i
+        b = j
+    else:
+        a = j
+        b = i
+
     n = len(tiles)
-    if tiles[a][0] > tiles[b][0] and tiles[a][1] > tiles[b][1]:
-        print("case 1")
+    ac = corners[a]
+    bc = corners[b]
 
-    if tiles[a][0] > tiles[b][0]:
-        if dirs[a] == 3 or dirs[b] == 1:
+    if tiles[b][1] > tiles[a][1]:
+        if bc in [1, 2, 3, 4]:
             return False
-        
-    if tiles[a][0] < tiles[b][0]:
-        if dirs[a] == 1 or dirs[b] == 3:
+        if ac in [0, 1, 2, 7]:
             return False
-
-    if tiles[a][1] > tiles[b][1]:
-        if dirs[a] == 0 or dirs[b] == 2:
+    else:
+        if bc in [0, 1, 3, 6]:
             return False
-        
-    if tiles[a][1] < tiles[b][1]:
-        if dirs[a] == 2 or dirs[b] == 0:
+        if ac in [0, 2, 3, 5]:
             return False
 
     return True
 
 
 def inside(tiles, a, b, c):
-    if tiles[c][0] <= min(tiles[a][0], tiles[b][0]):
-        return False
-    if tiles[c][0] >= max(tiles[a][0], tiles[b][0]):
-        return False
-    if tiles[c][1] <= min(tiles[a][1], tiles[b][1]):
-        return False
-    if tiles[c][1] >= max(tiles[a][1], tiles[b][1]):
+    a0 = tiles[a][0]
+    a1 = tiles[a][1]
+    b0 = tiles[b][0]
+    b1 = tiles[b][1]
+    c0 = tiles[c][0]
+    c1 = tiles[c][1]
+    n = len(tiles)
+    d0 = tiles[(c + 1) % n][0]
+    d1 = tiles[(c + 1) % n][1]
+
+    if c0 < min(a0, b0) or c0 > max(a0, b0) or c1 < min(a1, b1) or c1 > max(a1, b1):
         return False
 
-    return True
+    if c0 > min(a0, b0) and c0 < max(a0, b0) and c1 > min(a1, b1) and c1 < max(a1, b1):
+        return True
+
+    if c0 in [min(a0, b0), max(a0, b0)] and c1 in [min(a1, b1), max(a1, b1)]:
+        return False
+
+    if c0 == min(a0, b0) and tiles[(c + 1)%n][0] > c0:
+        return True
+    if c0 == min(a0, b0) and tiles[(c - 1)%n][0] > c0:
+        return True
+    if c0 == max(a0, b0) and tiles[(c + 1)%n][0] < c0:
+        return True
+    if c0 == max(a0, b0) and tiles[(c - 1)%n][0] < c0:
+        return True
+
+    if c1 == min(a1, b1) and tiles[(c + 1)%n][1] > c1:
+        return True
+    if c1 == min(a1, b1) and tiles[(c - 1)%n][1] > c1:
+        return True
+    if c1 == max(a1, b1) and tiles[(c + 1)%n][1] < c1:
+        return True
+    if c1 == max(a1, b1) and tiles[(c - 1)%n][1] < c1:
+        return True
+
+    return False
+
+
+def crosses(a, b, c, d):
+    x0 = min(a[0], b[0])
+    x1 = max(a[0], b[0])
+    y0 = min(a[1], b[1])
+    y1 = max(a[1], b[1])
+
+    if c[0] == d[0] and c[0] > x0 and c[0] < x1:
+        if min(c[1], d[1]) < y0 and max(c[1], d[1]) > y1:
+            return True
+
+    if c[1] == d[1] and c[1] > y0 and c[1] < y1:
+        if min(c[0], d[0]) < x0 and max(c[0], d[0]) > x1:
+            return True
+
+    return False
 
 
 def __main__():
@@ -77,7 +153,7 @@ def __main__():
     part2 = 0
     tiles = []
     areas = []
-    dirs = [-1]
+    corners = []
     pointed = -1
     turns = 0
 
@@ -90,28 +166,23 @@ def __main__():
             newdir = pdir(tile, newtile)
             if pointed < 0:
                 pointed = newdir
-                dirs.append(-1)
             else:
                 turns += turn(pointed, newdir)
                 pointed = newdir
-                dirs.append(newdir)
-            print(newtile, tile, pointed, turns)
         tiles.append(newtile)
-    dirs[0] = newdir
 
-    print(turns)
     part1 = max(areas)
 
     print(f"Part 1: {part1}")
+    n = len(tiles)
 
     areas = []
-    print
+    for i in range(len(tiles)):
+        corners.append(corner(tiles, i))
 
     for i in range(1, len(tiles)):
         for j in range(i):
-            if (i - j) % 2 != 0:
-                continue
-            if isopposite(tiles, dirs, i, j):
+            if isopposite(tiles, corners, i, j):
                 area = dist(tiles[i], tiles[j])
                 if area > part2:
                     for k in range(len(tiles)):
@@ -120,12 +191,13 @@ def __main__():
                         if inside(tiles, i, j, k):
                             area = 0
                             break
+                        if crosses(tiles[i], tiles[j], tiles[k], tiles[(k + 1) % n]):
+                            area = 0
+                            break
                 if area > part2:
-                    print(area, tiles[i], tiles[j])
                     part2 = area
 
     print(f"Part 2: {part2}")
-    # 4600181596 High
 
 
 if __name__ == '__main__':
