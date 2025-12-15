ee9488f17caea5d3c6042b6bc3c48ca3f128e37d
Paul Mc Auley <paul.mcauley@workday.com>
2025-12-15 19:23:46 +0000

    Interim Day 10

--- day10.py
+++ day10.py
@@ -33,25 +33,48 @@ def jcheck(target, jolt):
 def find_presses(target, buttons):
     count = 0
     wave = [list(target)]
+    jolted = [[0] * len(target)]
     presses = [-1]
 
     while True:
         count += 1
         nextwave = []
         nextpress = []
+        nextjolt = []
 
         for i in range(len(wave)):
             for j in range(presses[i] + 1, len(buttons)):
                 newtarget = press(wave[i], buttons[j])
-                if sum(newtarget) == 0:
-                    return count
+                newjolt = press2(jolted[i], buttons[j])
+                if max(newtarget) == 0:
+                    print('J', newjolt)
+                    return count, newjolt
                 nextwave.append(newtarget)
                 nextpress.append(j)
+                nextjolt.append(newjolt)
         wave = nextwave
         presses = nextpress
+        jolted = nextjolt
 
 
-def find_presses2(target, jolt, buttons):
+def find_presses2a(jolt, buttons):
+    count = 0
+
+    if max(jolt) == 0:
+        return 0
+
+    c, j = find_presses([(x % 1) for x in jolt], buttons)
+    count += c
+    print('Before', jolt)
+    jolt = [jolt[i] - j[i] for i in range(len(jolt))]
+    print('After', jolt)
+    count += 2 * find_presses2a([(x >> 1) for x in jolt], buttons)
+    print(count)
+
+    return count
+
+
+def find_presses2(jolt, buttons):
     count = 0
     wave = [[0] * len(jolt)]
 
@@ -74,28 +97,31 @@ def find_presses2(target, jolt, buttons):
         wave = nextwave
 
 
-def nttest():
-    a1 = np.array([
-        [0, 0, 0, 1],
-        [0, 1, 0, 1],
-        [0, 0, 1, 0],
-        [0, 0, 1, 1],
-        [1, 0, 1, 0],
-        [1, 1, 0, 0]
-    ])
+def lintest(jolt, buttons):
+    arrin = []
+    res = [0] * len(jolt)
 
-    a = np.array([
-        [0, 0, 0, 0, 1, 1],
-        [0, 1, 0, 0, 0, 1],
-        [0, 0, 1, 1, 1, 0],
-        [1, 1, 0, 1, 0, 0]
-    ])
+    for _ in range(len(jolt)):
+        arrin.append([0] * len(buttons))
 
-    b = np.array([3, 5, 4, 7])
+    for i in range(len(buttons)):
+        for x in buttons[i]:
+            arrin[x][i] = 1
 
+    a = np.array(arrin, dtype=int)
+    b = np.array(jolt)
+    ai = np.linalg.pinv(a)
+    bi = np.dot(ai, b)
+    print(ai)
+    print(bi)
     x = np.linalg.lstsq(a, b)
-
-    print(x)
+    print(x[0])
+    for i in range(len(buttons)):
+        for y in buttons[i]:
+            res[y] += int(x[0][i])
+    print(res)
+    print(jolt)
+    print('----')
 
 
 def __main__():
@@ -106,8 +132,10 @@ def __main__():
         target = [0 if x == '.' else 1 for x in line[1:].split(']')[0]]
         buttons = [[int(y) for y in x[1:-1].split(',')] for x in line.split('] ')[1].split(' ')[:-1]]
         jolt = [int(x) for x in line.split('{')[1][:-1].split(',')]
-        part1 += find_presses(target, buttons)
-        part2 += find_presses2(target, jolt, buttons)
+        c, _ = find_presses(target, buttons)
+        part1 += c
+        part2 += find_presses2a(jolt, buttons)
+        lintest(jolt, buttons)
 
     print(f"Part 1: {part1}")
     print(f"Part 2: {part2}")
